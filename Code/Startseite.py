97d368b1da725c959e38831c93167a8f4a62fa2a
Chris-0303 <eggench1@students.zhaw.ch>
2025-04-01 20:39:17 +0200

    Files from ZHAW Github

---
+++ Code/Startseite.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Willkommen zu Racing Insights!")
+st.write("VDSS Visualisierungsprojekt von Natalie, Sarruja, Nelly und Chris")
+st.write("Projekt-GitHub inkl. Dokumentation: https://github.zhaw.ch/sabessar/Racing_Insights")
+st.write("Dokumentation des Python-Pakets: https://docs.fastf1.dev/")
