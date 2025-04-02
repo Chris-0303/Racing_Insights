87216b7705e7aa70622188ab6199a5f58343d86e
Chris-0303 <eggench1@students.zhaw.ch>
2025-04-02 16:44:51 +0200

    Update Positionsverlauf.py

--- Code/pages/Positionsverlauf.py
+++ Code/pages/Positionsverlauf.py
@@ -14,43 +14,42 @@ st.subheader("Filtere Jahr/Rennen um den Positionsverlauf zu sehen")
 year = st.selectbox("Wähle eine Saison", [2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
                       index=None, placeholder="Saison")
 
-#load race calendar for that year
-calendar = load_races(year)
+if year: #only continue in code once year has been chosen by user
+    #load race calendar for that year
+    calendar = load_races(year)
 
-#ask user to choose race from that year
-race_str = st.selectbox("Wähle ein Rennen", calendar['CustomEventName'], index=None)
+    #ask user to choose race from that year
+    race_str = st.selectbox("Wähle ein Rennen", calendar['CustomEventName'], index=None)
 
-#convert race string to race number (expecting just one value to be correct, therefore using iloc[0])
-race_nr = calendar.loc[calendar['CustomEventName'] == race_str, 'RoundNumber'].iloc[0]
+    if race_str: #only continue in code once race has been chosen by user
+        #convert race string to race number (expecting just one value to be correct, therefore using iloc[0])
+        race_nr = calendar.loc[calendar['CustomEventName'] == race_str, 'RoundNumber'].iloc[0]
 
-#load data of the chosen race
-dat = load_data(year, race_nr)
+        with st.spinner("Daten werden geladen ..."):
+            #load data of the chosen race
+            dat = load_data(year, race_nr)
 
-#prepare data for visualization
-driver_info, laps = data_cleaner(dat)
+            #prepare data for visualization
+            driver_info, laps = data_cleaner(dat)
 
-#Vizualize (inspired by example from fastf1 documentation)
-fig, ax = plt.subplots(figsize=(8, 6))
+        #Vizualize (inspired by example from fastf1 documentation)
+        fig, ax = plt.subplots(figsize=(8, 6))
 
-for drv in dat.drivers:
-    drv_laps = dat.laps.pick_drivers(drv)
+        for drv in dat.drivers:
+            drv_laps = laps.pick_drivers(drv)
 
-    abb = drv_laps['Driver'].iloc[0]
-    style = fastf1.plotting.get_driver_style(identifier=abb,
-                                             style=['color', 'linestyle'],
-                                             session=dat)
+            abb = drv_laps['Driver'].iloc[0]
+            style = fastf1.plotting.get_driver_style(identifier=abb, style=['color', 'linestyle'], session=dat)
+            ax.plot(drv_laps['LapNumber'], drv_laps['Position'], label=abb, **style)
 
-    ax.plot(drv_laps['LapNumber'], drv_laps['Position'],
-            label=abb, **style)
+        #set ax properties
+        ax.set_ylim([20.5, 0.5])
+        ax.set_yticks([1, 5, 10, 15, 20])
+        ax.set_xlabel('Lap')
+        ax.set_ylabel('Position')
 
-#set ax properties
-ax.set_ylim([20.5, 0.5])
-ax.set_yticks([1, 5, 10, 15, 20])
-ax.set_xlabel('Lap')
-ax.set_ylabel('Position')
-
-#add legend
-ax.legend(bbox_to_anchor=(1.0, 1.02))
-plt.tight_layout()
-plt.show()
+        #add legend
+        ax.legend(bbox_to_anchor=(1.0, 1.02))
+        plt.tight_layout()
+        plt.show()
 
