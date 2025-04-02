bc90191087f6c9759dc95de651d11c7a32067320
Chris-0303 <eggench1@students.zhaw.ch>
2025-04-02 17:29:35 +0200

    Update helper_functions.py

--- Code/utils/helper_functions.py
+++ Code/utils/helper_functions.py
@@ -16,8 +16,8 @@ def load_races(year: int):
     calendar['CustomEventName'] = np.where(
         calendar.apply(lambda row: re.search(r'\b' + re.escape(row['Location']) + r'\b', row['EventName'], re.IGNORECASE),
                        axis=1),
-        "Race " + calendar['RoundNumber'].astype(str) + " - " + calendar['EventName'],
-        "Race " + calendar['RoundNumber'].astype(str) + " - " + calendar['EventName'] + " - " + calendar['Location']
+        "Rennen " + calendar['RoundNumber'].astype(str) + " - " + calendar['EventName'],
+        "Rennen " + calendar['RoundNumber'].astype(str) + " - " + calendar['EventName'] + " - " + calendar['Location']
     )
 
     #limit races on calendar if year is 2025
