2727f5f93f7601dc74ddfb2d1e9fdac546d5cfad
Natalie <nataliejakab@gmail.com>
2025-04-18 21:48:18 +0200

    -updates Gschwindigkeit

--- Code/pages/Geschwindigkeit.py
+++ Code/pages/Geschwindigkeit.py
@@ -11,7 +11,7 @@ from fastf1 import plotting
 plotting.setup_mpl()
 
 # --- User selections ---
-st.title("F1 Speed Map Visualisierung")
+st.title("F1 Speed Map Visualisierung von der schnellsten Runde")
 
 # Jahr auswählen
 year = st.selectbox("Wähle eine Saison", [2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
@@ -31,53 +31,70 @@ if year: #only continue in code once year has been chosen by user
         #race name for viz
         race_name = calendar.loc[calendar['CustomEventName'] == race_str, 'EventName'].iloc[0]
 
-        # Session auswählen
-        session = st.selectbox("Wähle eine Session", ['FP1', 'FP2', 'FP3', 'Q', 'S', 'R'],
-                               index=None, placeholder="Session auswählen")
+        # Session laden
+        sess = ff1.get_session(year, race_name, 'R')
+        sess.load()
 
-        if session:
-            # Session laden
-            sess = ff1.get_session(year, race_name, session)
-            sess.load()
+        # Fahrer-Auswahl aus tatsächlichen Teilnehmern
+        drivers = sorted(sess['CustomDriverName'].tolist())
+        driver = st.multiselect("Wähle 2 oder 4 Fahrer zum Vergleich:", options=drivers, default=[])
 
-            # Fahrer-Auswahl aus tatsächlichen Teilnehmern
-            drivers = sess.laps['Driver'].unique().tolist()
-            drivers.sort()
-            driver = st.selectbox("Wähle einen Fahrer", drivers, index=None, placeholder="Fahrer auswählen")
+        if drivers_str: #only continue in code once driver(s) have been chosen by user
+            #print warning and stop code execution if not 2 or 4 drivers are selected
+            if len(driver) not in (2, 4):
+                st.warning(f"Achtung: Wähle 2 oder 4 Fahrer zum Vergleich")
+                st.stop()
 
-            if driver:
-                lap = sess.laps.pick_drivers(driver).pick_fastest()
+            #find abbreviations of selected drivers in driver_info
+            drivers_abbr = drivers.loc[drivers['CustomDriverName'].isin(driver), 'Abbreviation'].tolist()
 
-                # Telemetriedaten
-                x = lap.telemetry['X']
-                y = lap.telemetry['Y']
-                color = lap.telemetry['Speed']
+            #calc number of rows need in viz based on drivers_amount
+            rows = 1 if len(driver) == 2 else 2
 
-                # Linien-Segmente vorbereiten
-                points = np.array([x, y]).T.reshape(-1, 1, 2)
-                segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
-                # Plot erstellen
-                fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(12, 6.75))
-                fig.suptitle(f'{race_name} {year} - {driver} - Speed', size=24, y=0.97)
+        if driver:
+            #print warning and stop code execution if not 2 or 4 drivers are selected
+            if len(driver) not in (2, 4):
+                st.warning(f"Achtung: Wähle 2 oder 4 Fahrer zum Vergleich")
+                st.stop()
 
-                plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.12)
-                ax.axis('off')
+            #find abbreviations of selected drivers in driver_info
+            drivers_abbr = drivers.loc[drivers['CustomDriverName'].isin(driver), 'Abbreviation'].tolist()
 
-                # Hintergrund-Track
-                ax.plot(x, y, color='black', linestyle='-', linewidth=16, zorder=0)
+            #calc number of rows need in viz based on drivers_amount
+            rows = 1 if len(driver) == 2 else 2
+            lap = sess.laps.pick_drivers(driver).pick_fastest()
 
-                # Farbverlauf für Speed
-                norm = plt.Normalize(color.min(), color.max())
-                colormap = mpl.cm.plasma
-                lc = LineCollection(segments, cmap=colormap, norm=norm, linestyle='-', linewidth=5)
-                lc.set_array(color)
-                ax.add_collection(lc)
+            # Telemetriedaten
+            x = lap.telemetry['X']
+            y = lap.telemetry['Y']
+            color = lap.telemetry['Speed']
 
-                # Farbskala
-                cbaxes = fig.add_axes([0.25, 0.05, 0.5, 0.05])
-                normlegend = mpl.colors.Normalize(vmin=color.min(), vmax=color.max())
-                mpl.colorbar.ColorbarBase(cbaxes, norm=normlegend, cmap=colormap, orientation="horizontal")
+            # Linien-Segmente vorbereiten
+            points = np.array([x, y]).T.reshape(-1, 1, 2)
+            segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
-                # Plot anzeigen
-                st.pyplot(fig)
+            # Plot erstellen
+            fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(12, 6.75))
+            fig.suptitle(f'{race_name} {year} - {driver} - Speed', size=24, y=0.97)
+
+            plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.12)
+            ax.axis('off')
+
+            # Hintergrund-Track
+            ax.plot(x, y, color='black', linestyle='-', linewidth=16, zorder=0)
+
+            # Farbverlauf für Speed
+            norm = plt.Normalize(color.min(), color.max())
+            colormap = mpl.cm.plasma
+            lc = LineCollection(segments, cmap=colormap, norm=norm, linestyle='-', linewidth=5)
+            lc.set_array(color)
+            ax.add_collection(lc)
+
+            # Farbskala
+            cbaxes = fig.add_axes([0.25, 0.05, 0.5, 0.05])
+            normlegend = mpl.colors.Normalize(vmin=color.min(), vmax=color.max())
+            mpl.colorbar.ColorbarBase(cbaxes, norm=normlegend, cmap=colormap, orientation="horizontal")
+
+            # Plot anzeigen
+            st.pyplot(fig)
